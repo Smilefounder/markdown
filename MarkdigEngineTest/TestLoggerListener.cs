a295a738cedd864c0e651656f645deac081f790c
Qinen Zhu <qinzhu@microsoft.com>
2017-09-04 17:27:37 +0800

    Add logger listener for testing

---
+++ MarkdigEngineTest/TestLoggerListener.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Microsoft.DocAsCode.Common;
+
+namespace MarkdigEngine.Test
+{
+    internal class TestLoggerListener : ILoggerListener
+    {
+        private readonly Func<ILogItem, bool> _filter;
+        public List<ILogItem> Items { get; } = new List<ILogItem>();
+
+        public TestLoggerListener(Func<ILogItem, bool> filter)
+        {
+            _filter = filter ?? throw new ArgumentNullException(nameof(filter));
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public void Flush()
+        {
+        }
+
+        public void WriteLine(ILogItem item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException(nameof(item));
+            }
+
+            if (_filter(item))
+            {
+                Items.Add(item);
+            }
+        }
+    }
+}
