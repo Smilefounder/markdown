20646a2f68f0e8094891418655f18f42ae97bd85
Qinen Zhu <qinzhu@microsoft.com>
2017-09-04 17:26:39 +0800

    Add MarkdigEngine.Plugin project

---
+++ MarkdigEngine.Plugin/IMarkdownTokenTreeValidator.cs
@@ -0,0 +1,9 @@
+﻿using Markdig.Syntax;
+
+namespace MarkdigEngine.Plugin
+{
+    public interface IMarkdownTokenTreeValidator
+    {
+        void Validate(MarkdownDocument document);
+    }
+}
---
+++ MarkdigEngine.Plugin/IMarkdownTokenValidatorProvider.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Immutable;
+
+namespace MarkdigEngine.Plugin
+{
+    public interface IMarkdownTokenValidatorProvider
+    {
+        ImmutableArray<IMarkdownTokenValidator> GetValidators();
+    }
+}
