121c160f88846486abd241b475fce2d9a8c9c069
Qinen Zhu <qinezh@users.noreply.github.com>
2017-11-09 00:54:24 -0600

    Skip ut for emoij (#68)

--- MarkdigEngine.Tests/EmojiTest.cs
+++ MarkdigEngine.Tests/EmojiTest.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xunit;
+﻿using Xunit;
 
 namespace MarkdigEngine.Tests
 {
     public class EmojiTest
     {
-        [Fact]
+        [Fact(Skip = "Disable Emoij")]
         public void EmojiTestGeneral()
         {
             //arange
